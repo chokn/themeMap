c3214f010559b8376b9719d213be18782d662f7d
chokn <cho.kenneth@bls.gov>
2015-10-27 15:25:47 -0400

    Add a bunch of different types of choropleths

--- map.R
+++ map.R
@@ -26,7 +26,7 @@ intervals = function(.df, ...){
   argList = match.call(expand.dots=FALSE)$...
   for(i in 1:length(argList)){
     colName <- argList[[i]]
-    series_colName = eval(substitute(coLName), envir = .df, enclos=parent.frame())
+    series_colName = eval(substitute(colName), envir = .df, enclos=parent.frame())
     min <- min(series_colName)
     max <- max(series_colName)
     diff <- max - min
@@ -94,3 +94,72 @@ pop48 <- remove.farflung(pop)
 
 #add fields for alternative breaks of population categories
 pop48 = intervals(pop48, pop)
+
+map48 <- merge(us48, pop48)
+
+#plotting the map
+b <- ggplot(data = map48, aes(x=long, y=lat, group = group))
+
+d <- b + geom_polygon(aes(x=long, y=lat, group = group), color = "dark grey", size = 0.3)
+
+# Change default fill to white. add map projection
+w <- d + geom_polygon(aes(x=long, y = lat, group = group), color = "dark grey", size = 0.3, fill = "white")
+w
+
+p = w + poly
+p
+
+# title and stuff
+my_title <- "Population map of 48 contiguous states andd the District of Columbia,\ndefault settings"
+sources <- "Source: U.S. Census Bureau 2014 Population Estimates http://1.usa.gov/1LPM7hc\n and Cartographic Boundary file (States, cb_2014_us_state_5m) http://1.usa.gov/1IZgFLV\n Prepared by: @technocrat 2015-07-15"
+
+# Add population data to map
+p <- b + geom_polygon(color = "dark grey", size = 0.3, aes(fill = pop)) + ggtitle(my_title) + annotate("text", x = -100, y = 22, label = sources, size = 3, family = "Times", color = "black")
+p
+
+#remove background and axis legends, add projection
+my_title <- "Population Map of 48 Contiguous States and District of Columbia,\n no background or axis legends, polyconic projection"
+p +  plain_theme + no_ylab + no_xlab + poly + ggtitle(my_title) + annotate("text", x = -100, y = 22, label = sources,  size = 3, family = "Times", color = "black")
+
+# Disrete scale
+buckets = bucket(pop48$pop, 5000000)
+popb = data.frame(pop48$id, buckets)
+names(popb) = c("id", "buckets")
+map48b <- merge(us48, popb)
+
+my_title <- "Population Map of 48 Contiguous States and District of Columbia,\n manual discrete scale, default colors"
+m <- b + geom_polygon(color = "dark grey", size = 0.3, aes(fill = factor(map48b$buckets)))
+
+m + plain_theme + no_xlab + no_ylab + poly + ggtitle(my_title) + annotate("text", x = -100, y = 22, label = sources, size = 3, family = "Times", color = "black")
+
+#different color scheme (graded neutral)
+
+my_title = "Population Map of 48 Contiguous States and District of Columbia,\n manual discrete scale, graded neutral colors"
+m + plain_theme + no_xlab + no_ylab + poly + ggtitle(my_title) + annotate("Text", x = -100, y = 22, label = sources, size = 3, family = "Times", color = "black") + scale_fill_brewer(type = "seq", palette = "YlOrBr")
+
+# better legend
+my_title = "Population Map of 48 Contiguous States and District of Columbia,\nwith improved legend"
+m + plain_theme + no_xlab + no_ylab + poly + ggtitle(my_title) + annotate("text", x = -100, y = 22, label = sources, size = 3, family = "Times", color = "black") + scale_fill_brewer(type = "seq", palette = "YlOrBr", name = "State Population", labels=c("Under 5 million", "5-10 million", "10-15 million", "15-20 million", "20-25 million", "more than 35 million"))
+
+# with equal interval scale
+my_title = "Population Map of 48 Contiguous States and District of Columbia,\nwith equal interval scale"
+e <- b + geom_polygon(color = "dark grey", size = 0.3, aes(fill = pop.equal)) + ggtitle(my_title) + annotate("text", x = -100, y = 22, label = sources, size = 3, family = "Times", color = "black") + plain_theme + no_xlab + no_ylab + poly + scale_fill_brewer(type = "seq", palette = "YlOrBr", name = "State Population", labels = c("Under 6.95 million", "6.95 - 13.3 million", "19.7 - 26.1 million", "26.1 - 32.4 million", "34.2-38.8 million"))
+e                                                                                                             
+
+# quantile interval
+my_title = "Population Map of 48 Contiguous States and District of Columbia,\nwith quantile interval scale"
+q <- b + geom_polygon(aes(x = long, y = lat, group = group, fill = pop.quantile), color = "dark grey", size = 0.3) 
+q <- q + plain_theme + no_ylab + no_xlab + poly + scale_fill_brewer(palette = "YlOrBr", name = "State Population", labels=c("Under 1.33 million","1.33-2.94 million","2.94-4.65 million","4.65-6.55  million","6.55-9.94 million", "9.94-38.8 million")) + ggtitle(my_title)
+q
+
+#standard deviation scale
+my_title = "Population Map of 48 Contiguous States and District of Columbia,\nwith standard deviation scale"
+s = b + geom_polygon(aes(x=long, y=lat, group=group, fill=pop.std), color = "dark grey", size = 0.3)
+s = s + plain_theme + no_ylab + no_xlab + poly + scale_fill_brewer(palette = "YlOrBr", name = "State Population", labels=c("Under 7.77 million","7.77-15 million","15-22.2 million","22.2-29.3 million","36.5-38.8 million")) + ggtitle(my_title)
+s
+
+#natural scale
+my_title = "Population Map of 48 Contiguous States and District of Columbia,\nwith natural scale"
+n = b + geom_polygon(aes(x=long, y=lat, group=group, fill=pop.natural), color = "dark grey", size = 0.3)
+n = n + plain_theme + no_ylab + no_xlab + poly + scale_fill_brewer(palette = "YlOrBr", name = "State Population", labels=c("Under 3.6 million","3.6-7.06 million","7.06-12.9 million","12.9-19.9 million","19.9-27 million","27-38.8 million")) + ggtitle(my_title)
+n
